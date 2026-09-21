13b4778b628f6ef602b2b715d491e5738d883704
anchundiatech <alejodev2022@gmail.com>
2026-09-21 13:25:29 -0500

    feat(amenities): add amenities data and context

---
+++ frontend/src/modules/amenities/data/amenities.data.js
@@ -0,0 +1,4 @@
+export const AMENITIES = [
+  { id: 1, buildingId: 1, name: "Salón de Eventos", capacity: 50, openTime: "08:00", closeTime: "23:00" },
+  { id: 2, buildingId: 1, name: "Parrilla Terraza", capacity: 15, openTime: "10:00", closeTime: "22:00" },
+];
